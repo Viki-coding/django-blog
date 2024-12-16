f632fb1d4826895c2b1d0b747739fcc3b2912a92
Viki Mulhall <vikimulhall@gmail.com>
2024-12-16 11:22:58 +0000

    add meta data to posts

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vrzm9rslr1r!t5w+p0m5f8_b5f5jytw)v(&zombo*%8vhn92=7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["8000-vikicoding-djangoblog-l0bu3m538wp.ws.codeinstitute-ide.net", ".herokuapp.com"]
+ALLOWED_HOSTS = ["8000-vikicoding-djangoblog-l0bu3m538wp.ws.codeinstitute-ide.net", ".herokuapp.com", "8000-vikicoding-djangoblog-l0bu3m538wp.ws.codeinstitute-ide.net"]
 
 
 # Application definition
